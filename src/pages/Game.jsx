a99d137edebfc2c25b40468ccbdec958810909d0
Vinicius Jacques <vinicius-jacques@hotmail.com>
2022-02-14 19:07:37 -0300

    Update

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Game extends Component {
   render() {
     return (
-      <div>Game</div>
+      <div>
+        <h1>OLÁAAA</h1>
+      </div>
     );
   }
 }
