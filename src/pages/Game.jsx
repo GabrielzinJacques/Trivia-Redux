04f6a34caf51a83e5d741bd3d57defe3d5b6f957
Vinicius Jacques <vinicius-jacques@hotmail.com>
2022-02-14 19:32:09 -0300

    Requisito 3 concluido

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 export default class Game extends Component {
   render() {
     return (
-      <div>
-        <h1>OLÁAAA</h1>
-      </div>
+      <section />
     );
   }
 }
