88dbd4f5d353a5a735b78ec9f10000b0bcd3e464
Vinicius Jacques <vinicius-jacques@hotmail.com>
2022-02-17 17:36:13 -0300

    Requisito 10 update

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import GameQuestions from '../components/GameQuestions';
 import Header from '../components/Header';
 
 export default class Game extends Component {
   render() {
     const { history } = this.props;
+    console.log(history);
     return (
       <section>
         <Header />
@@ -13,3 +15,7 @@ export default class Game extends Component {
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.objectOf(PropTypes.string),
+}.isRequired;
