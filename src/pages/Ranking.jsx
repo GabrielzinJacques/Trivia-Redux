ab3dfc79cac60601d6120b63fbf74dbbdc9d5b99
Vinicius Jacques <vinicius-jacques@hotmail.com>
2022-02-17 22:05:52 -0300

    Requisito 17

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Ranking extends Component {
+  playAgain = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <section>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          onClick={ this.playAgain }
+          data-testid="btn-go-home"
+          type="button"
+        >
+          Jogar novamente
+
+        </button>
       </section>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any),
+}.isRequired;
