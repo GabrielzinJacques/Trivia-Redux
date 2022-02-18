36dd774ad86e379cff885cfb9af6a83a8c204bba
Vinicius Jacques <vinicius-jacques@hotmail.com>
2022-02-17 21:37:04 -0300

    Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,25 +5,32 @@ import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const THREE = 3;
     const verify = assertions < THREE;
     return (
-      <>
+      <section>
         <Header />
         {verify ? <p data-testid="feedback-text">Could be better...</p>
           : <p data-testid="feedback-text">Well Done!</p>}
-      </>
+        <div>
+          {console.log(score)}
+          <h3 data-testid="feedback-total-score">{score}</h3>
+          <h3 data-testid="feedback-total-question">{assertions}</h3>
+        </div>
+      </section>
     );
   }
 }
 
 Feedback.propTypes = {
   assertions: PropTypes.number,
+  score: PropTypes.number,
 }.isRequired;
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
